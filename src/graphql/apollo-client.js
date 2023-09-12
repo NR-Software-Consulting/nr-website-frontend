97b8dce749cb66fd4a9244c5d0fe0ec9fdfec0a2
Raza Mughal <razasabir23@outlook.com>
2023-09-12 23:12:13 +0500

    read baseurl from .env

--- src/graphql/apollo-client.js
+++ src/graphql/apollo-client.js
@@ -1,21 +1,22 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 import { createUploadLink } from "apollo-upload-client";
-
+const BaseURL =
+  process.env.NEXT_PUBLIC_BACKEND_URL || "https://admin.nrmobiles.com";
 const client = new ApolloClient({
-  uri: "https://admin.nrmobiles.com/graphql",
+  uri: `${BaseURL}/graphql`,
 
   cache: new InMemoryCache(),
 });
 export default client;
 const uploadLink = createUploadLink({
-  uri: "https://admin.nrmobiles.com/graphql",
+  uri: `${BaseURL}/graphql`,
 });
 export const uploadFileClient = new ApolloClient({
   link: uploadLink,
   cache: new InMemoryCache(),
 });
 export const apiConfig = {
-  baseUrlMedia: "https://admin.nrmobiles.com",
-  baseUrl: "https://admin.nrmobiles.com/graphql",
+  baseUrlMedia: `${BaseURL}`,
+  baseUrl: `${BaseURL}/graphql`,
   UPLOAD_FILE: "/api/upload",
 };
