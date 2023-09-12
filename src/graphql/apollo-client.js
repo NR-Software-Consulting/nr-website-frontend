218cf5efa699db22c1ad0e38eda9b01793e85d14
Raza Mughal <razasabir23@outlook.com>
2023-09-12 23:03:43 +0500

    delete .env from git

--- src/graphql/apollo-client.js
+++ src/graphql/apollo-client.js
@@ -2,20 +2,20 @@ import { ApolloClient, InMemoryCache } from "@apollo/client";
 import { createUploadLink } from "apollo-upload-client";
 
 const client = new ApolloClient({
-  uri: "https://cn.liveurdunews.com/graphql",
+  uri: "https://admin.nrmobiles.com/graphql",
 
   cache: new InMemoryCache(),
 });
 export default client;
 const uploadLink = createUploadLink({
-  uri: "https://cn.liveurdunews.com/graphql",
+  uri: "https://admin.nrmobiles.com/graphql",
 });
 export const uploadFileClient = new ApolloClient({
   link: uploadLink,
   cache: new InMemoryCache(),
 });
 export const apiConfig = {
-  baseUrlMedia: "https://cn.liveurdunews.com",
-  baseUrl: "https://cn.liveurdunews.com/graphql",
+  baseUrlMedia: "https://admin.nrmobiles.com",
+  baseUrl: "https://admin.nrmobiles.com/graphql",
   UPLOAD_FILE: "/api/upload",
 };
