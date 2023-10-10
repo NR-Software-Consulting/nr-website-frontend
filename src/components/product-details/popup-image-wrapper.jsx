1625bb965f912e8123ab81c2d8d92df31f5624dd
Raza Mughal <razasabir23@outlook.com>
2023-10-11 00:32:01 +0500

    fixed popup issue

--- src/components/product-details/popup-image-wrapper.jsx
+++ src/components/product-details/popup-image-wrapper.jsx
@@ -4,7 +4,7 @@ import { Box } from "@mui/material";
 const PopupThumbWrapper = ({ imageURLs }) => {
   return (
     <>
-      <div className="tp-product-details-thumb-wrapper tp-tab d-sm-flex">
+      <div className="tp-product-details-thumb-wrapper tp-tab d-sm-flex align-items-center">
         <div className="tab-content m-img">
           <div className="tab-pane fade show active">
             <div className="tp-product-details-nav-main-thumb p-relative">
