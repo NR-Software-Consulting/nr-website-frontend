899f31051e19cda8cbf79ca1b8048cd564f5218d
Raza Mughal <razasabir23@outlook.com>
2023-10-26 19:31:48 +0500

    new feature

--- src/components/shop/shop-filter/category-filter.jsx
+++ src/components/shop/shop-filter/category-filter.jsx
@@ -1,5 +1,3 @@
-
-
 import React from "react";
 import { useTranslations } from "next-intl";
 import { useQuery } from "@apollo/client";
@@ -15,7 +13,17 @@ const CategoryFilter = ({
   selectedSubCategories,
 }) => {
   const t = useTranslations("header");
-  const { data, loading, error } = useQuery(CATEGORIES_LIST)
+  const { data, loading, error } = useQuery(CATEGORIES_LIST);
+  const reorderedCategoriesList = [...categoriesList];
+  const selectedCategoriesList = reorderedCategoriesList.filter(
+    (categoryItem) => selectedCategories.includes(categoryItem?.id)
+  );
+  const unselectedCategoriesList = reorderedCategoriesList.filter(
+    (categoryItem) => !selectedCategories.includes(categoryItem?.id)
+  );
+  const finalCategoriesList = selectedCategoriesList.concat(
+    unselectedCategoriesList
+  );
   let content = null;
   if (!data) {
     content = <CategoryListLoader loading={true} />;
@@ -24,48 +32,71 @@ const CategoryFilter = ({
   } else {
     content = (
       <ul className="filter-items filter-checkbox">
-        {categoriesList?.map((categoryItem) => (
-          <li key={categoryItem?.id} className="filter-item checkbox">
-            <input
-              type="checkbox"
-              id={`category-${categoryItem?.id}`}
-              value={categoryItem?.id}
-              checked={selectedCategories?.includes(categoryItem?.id)}
-              onChange={() => handleCategoryFilter(categoryItem?.id, categoryItem?.attributes?.slug)}
-            />
-            <label
-              className="form-check-label"
-              htmlFor={`category-${categoryItem?.id}`}
-            >
-              {categoryItem?.attributes?.name}
-            </label>
-            {categoryItem?.attributes?.sub_categories?.data?.length > 0 && (
-              <ul className="sub-category-list ms-5">
-                {categoryItem.attributes?.sub_categories?.data?.map(
-                  (subCategory) => (
-                    <li key={subCategory?.id}>
-                      <input
-                        type="checkbox"
-                        id={`subcategory-${subCategory?.id}`}
-                        value={subCategory?.id}
-                        checked={selectedSubCategories?.includes(subCategory?.id)}
-                        onChange={() =>
-                          handleSubCategoryFilter(subCategory?.id, categoryItem?.id, categoryItem?.attributes?.slug, subCategory?.attributes?.slug, null, categoryItem)
-                        }
-                      />
-                      <label
-                        className="form-check-label"
-                        htmlFor={`subcategory-${subCategory?.id}`}
-                      >
-                        {subCategory?.attributes?.name}
-                      </label>
-                    </li>
+        {finalCategoriesList?.map((categoryItem) => {
+          const isSelectedCategory = selectedCategories?.includes(
+            categoryItem?.id
+          );
+
+          return (
+            <li key={categoryItem?.id} className="filter-item checkbox">
+              <input
+                type="checkbox"
+                id={`category-${categoryItem?.id}`}
+                value={categoryItem?.id}
+                checked={isSelectedCategory}
+                onChange={() =>
+                  handleCategoryFilter(
+                    categoryItem?.id,
+                    categoryItem?.attributes?.slug
                   )
+                }
+              />
+              <label
+                className="form-check-label"
+                htmlFor={`category-${categoryItem?.id}`}
+              >
+                {categoryItem?.attributes?.name}
+              </label>
+              {isSelectedCategory &&
+                categoryItem?.attributes?.sub_categories?.data?.length > 0 && (
+                  <ul className="sub-category-list ms-5">
+                    {categoryItem.attributes?.sub_categories?.data?.map(
+                      (subCategory) => {
+                        const isSelectedSubCategory =
+                          selectedSubCategories?.includes(subCategory?.id);
+                        return (
+                          <li key={subCategory?.id}>
+                            <input
+                              type="checkbox"
+                              id={`subcategory-${subCategory?.id}`}
+                              value={subCategory?.id}
+                              checked={isSelectedSubCategory}
+                              onChange={() =>
+                                handleSubCategoryFilter(
+                                  subCategory?.id,
+                                  categoryItem?.id,
+                                  categoryItem?.attributes?.slug,
+                                  subCategory?.attributes?.slug,
+                                  null,
+                                  categoryItem
+                                )
+                              }
+                            />
+                            <label
+                              className="form-check-label"
+                              htmlFor={`subcategory-${subCategory?.id}`}
+                            >
+                              {subCategory?.attributes?.name}
+                            </label>
+                          </li>
+                        );
+                      }
+                    )}
+                  </ul>
                 )}
-              </ul>
-            )}
-          </li>
-        ))}
+            </li>
+          );
+        })}
       </ul>
     );
   }
@@ -74,9 +105,7 @@ const CategoryFilter = ({
       <h3 className="tp-shop-widget-title">{t("Categories")}</h3>
       <div className="tp-shop-widget-content">
         <div className="tp-shop-widget-categories">
-          <div className="tp-shop-widget-checkbox">
-            {content}
-          </div>
+          <div className="tp-shop-widget-checkbox">{content}</div>
         </div>
       </div>
     </div>
