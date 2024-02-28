b05f37e7c3851ca1397a0fd496d80c07e034a939
Raza Mughal <razasabir23outlook.com>
2024-02-28 23:04:05 +0500

    adsense added

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,4 +1,149 @@
-module.exports = {
-  siteUrl: "https://nrmobiles.com",
-  generateRobotsTxt: true,
-};
+module.exports = [
+  {
+    siteUrl: "https://nrmobiles.com/",
+    generateRobotsTxt: true,
+  },
+  {
+    siteUrl: "https://nrmobiles.com/login",
+    generateRobotsTxt: true,
+  },
+  {
+    siteUrl: "https://nrmobiles.com/products",
+    generateRobotsTxt: true,
+  },
+  {
+    siteUrl: "https://nrmobiles.com/about",
+    generateRobotsTxt: true,
+  },
+  {
+    siteUrl: "https://nrmobiles.com/contact",
+    generateRobotsTxt: true,
+  },
+  {
+    siteUrl: "https://nrmobiles.com/wishlist",
+    generateRobotsTxt: true,
+  },
+  {
+    siteUrl: "https://nrmobiles.com/cart",
+    generateRobotsTxt: true,
+  },
+  {
+    siteUrl:
+      "https://nrmobiles.com/product/meiyulin-original-66-w-pd-power-bank",
+    generateRobotsTxt: true,
+  },
+  {
+    siteUrl:
+      "https://nrmobiles.com/product/rgb-light-speaker-with-wireless-charger",
+    generateRobotsTxt: true,
+  },
+  {
+    siteUrl: "https://nrmobiles.com/product/apple-airpods-gen-2",
+    generateRobotsTxt: true,
+  },
+  {
+    siteUrl: "https://nrmobiles.com/product/boya-k9-mic",
+    generateRobotsTxt: true,
+  },
+  {
+    siteUrl: "https://nrmobiles.com/product/samsung-32gb-usb-3-0",
+    generateRobotsTxt: true,
+  },
+  {
+    siteUrl: "https://nrmobiles.com/product/samsung-64gb-usb-3-0",
+    generateRobotsTxt: true,
+  },
+  {
+    siteUrl: "https://nrmobiles.com/product/p23-bluetooth-speaker",
+    generateRobotsTxt: true,
+  },
+  {
+    siteUrl: "https://nrmobiles.com/product/samsung-c-to-c-cable",
+    generateRobotsTxt: true,
+  },
+  {
+    siteUrl: "https://nrmobiles.com/product/apple-usb-to-ios-cable",
+    generateRobotsTxt: true,
+  },
+  {
+    siteUrl: "https://nrmobiles.com/product/samsung-usb-to-type-c-cable",
+    generateRobotsTxt: true,
+  },
+  {
+    siteUrl: "https://nrmobiles.com/product/led-crystal-touch-lamp",
+    generateRobotsTxt: true,
+  },
+  {
+    siteUrl: "https://nrmobiles.com/product/ultrapod-max-airbuds-air31",
+    generateRobotsTxt: true,
+  },
+  {
+    siteUrl: "https://nrmobiles.com/product/lenovo-he05",
+    generateRobotsTxt: true,
+  },
+  {
+    siteUrl: "https://nrmobiles.com/forgot",
+    generateRobotsTxt: true,
+  },
+  {
+    siteUrl: "https://nrmobiles.com/register",
+    generateRobotsTxt: true,
+  },
+  {
+    siteUrl: "https://nrmobiles.com/product/touch-lamp-bluetooth-speaker",
+    generateRobotsTxt: true,
+  },
+  {
+    siteUrl: "https://nrmobiles.com/product/jbl-bluetooth-speaker",
+    generateRobotsTxt: true,
+  },
+  {
+    siteUrl: "https://nrmobiles.com/product/apple-airpods-pro",
+    generateRobotsTxt: true,
+  },
+  {
+    siteUrl: "https://nrmobiles.com/product/samsung-galaxy-buds-2",
+    generateRobotsTxt: true,
+  },
+  {
+    siteUrl: "https://nrmobiles.com/product/apple-airpods-gen-3",
+    generateRobotsTxt: true,
+  },
+  {
+    siteUrl: "https://nrmobiles.com/product/apple-airpods-pro-2",
+    generateRobotsTxt: true,
+  },
+  {
+    siteUrl: "https://nrmobiles.com/product/boya-m1-mic",
+    generateRobotsTxt: true,
+  },
+  {
+    siteUrl: "https://nrmobiles.com/product/boya-k8-mic",
+    generateRobotsTxt: true,
+  },
+  {
+    siteUrl: "https://nrmobiles.com/product/apple-c-to-ios-cable",
+    generateRobotsTxt: true,
+  },
+  {
+    siteUrl: "https://nrmobiles.com/product/oneplus-usb-to-c-cable",
+    generateRobotsTxt: true,
+  },
+  {
+    siteUrl: "https://nrmobiles.com/product/oneplus-c-to-c-cable",
+    generateRobotsTxt: true,
+  },
+  {
+    siteUrl: "https://nrmobiles.com/product/samsung-micro-cable",
+    generateRobotsTxt: true,
+  },
+  {
+    siteUrl: "https://nrmobiles.com/product/huawei-micro-cable",
+    generateRobotsTxt: true,
+  },
+  {
+    siteUrl:
+      "https://nrmobiles.com/product/apple-c-to-c-original-for-15-series",
+    generateRobotsTxt: true,
+  },
+];
